1f8df310f7b41d3524f824dd4ebce2c7e517e3bf
nickciava98 <n.ciavarella@outlook.it>
2022-09-08 08:42:15 +0200

    Added florence_controllers

---
+++ florence_controllers/README.md
@@ -0,0 +1,18 @@
+Florence Controllers
+====================
+
+Description
+-----------
+Florence customizations for quick creates and quick edits.
+
+Usage
+-----
+* Change the attributes in specific fields
+
+Author
+------
+* Niccolò Ciavarella <niccolo@florenceorganics.com>
+
+Contributors
+------------
+* Florence Organics Ltd
---
+++ florence_controllers/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Florence Controllers",
+    'summary': "Florence customizations for quick creates and quick edits",
+    'license': 'OPL-1',
+    'author': "Niccolò Ciavarella",
+    'category': '',
+    'version': '14.0.1',
+    'website': "http://www.nciavarella.me",
+    'depends': ['sale', 'sale_management'],
+    'data': ['views/sale_order_form.xml',
+             'views/account_move_form.xml'],
+    'application': False,
+    'installable': True,
+}
