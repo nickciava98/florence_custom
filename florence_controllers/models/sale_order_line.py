ba8833433b8b7ad916b24647a58707f03ed40ede
nickciava98 <n.ciavarella@outlook.it>
2022-09-20 11:48:59 +0200

    Fix controllers

--- florence_controllers/models/sale_order_line.py
+++ florence_controllers/models/sale_order_line.py
@@ -1,12 +1,14 @@
 from odoo import models, fields, api, exceptions
 
+
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.constrains("tax_id")
     def constrains_tax_id(self):
         for line in self:
-            if not line.display_type and len(line.tax_id) == 0:
+            if not line.display_type and line.product_id.default_code != "Free Sample" \
+                    and len(line.tax_id) == 0:
                 raise exceptions.ValidationError(
                     "VAT must be present in sale order line!"
                 )
