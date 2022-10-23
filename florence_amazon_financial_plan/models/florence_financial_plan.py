abd40db472083bfab9ad3d42e88de33455c87970
nickciava98 <n.ciavarella@outlook.it>
2022-10-23 11:54:08 +0200

    Improved FP Florence

--- florence_amazon_financial_plan/models/florence_financial_plan.py
+++ florence_amazon_financial_plan/models/florence_financial_plan.py
@@ -15,14 +15,20 @@ class FlorenceFinancialPlan(models.Model):
         required = True
     )
     gi = fields.Float(
-        string = "G.I."
+        string = "G.I.",
+        compute = "_compute_gi"
     )
-    vat = fields.Float()
+    vat = fields.Float(
+        compute = "_compute_vat"
+    )
     cgi = fields.Float(
         string = "C.G.I.",
         compute = "_compute_cgi"
     )
-    perc = fields.Float()
+    perc = fields.Float(
+        string = "10% C.G.I.",
+        compute = "_compute_perc"
+    )
     monthly_total = fields.Float(
         compute = "_compute_monthly_total"
     )
@@ -104,10 +110,66 @@ class FlorenceFinancialPlan(models.Model):
         "res.currency",
         compute = "_compute_currency_id"
     )
+    amz_total_it = fields.Float(
+        default = 0
+    )
+    amz_vat_it = fields.Float(
+        default = 0
+    )
+    amz_total_fr = fields.Float(
+        default = 0
+    )
+    amz_vat_fr = fields.Float(
+        default = 0
+    )
+    amz_total_de = fields.Float(
+        default = 0
+    )
+    amz_vat_de = fields.Float(
+        default = 0
+    )
+    amz_total_es = fields.Float(
+        default = 0
+    )
+    amz_vat_es = fields.Float(
+        default = 0
+    )
+    amz_total_uk = fields.Float(
+        default = 0
+    )
+    amz_vat_uk = fields.Float(
+        default = 0
+    )
+
+    @api.depends("amz_total_it", "amz_total_fr",
+                 "amz_total_de", "amz_total_es",
+                 "amz_total_uk")
+    def _compute_gi(self):
+        for line in self:
+            line.gi = line.amz_total_it + line.amz_total_fr \
+                      + line.amz_total_de + line.amz_total_es \
+                      + line.amz_total_uk
+
+    @api.depends("amz_vat_it", "amz_vat_fr",
+                 "amz_vat_de", "amz_vat_es",
+                 "amz_vat_uk")
+    def _compute_vat(self):
+        for line in self:
+            line.vat = line.amz_vat_it + line.amz_vat_fr \
+                       + line.amz_vat_de + line.amz_vat_es \
+                       + line.amz_vat_uk
 
     def _compute_currency_id(self):
         for line in self:
             line.currency_id = self.env.ref("base.main_company").currency_id
+
+    @api.depends("cgi")
+    def _compute_perc(self):
+        for line in self:
+            line.perc = 0
+
+            if line.cgi:
+                line.perc = 0.1 * line.cgi
 
     @api.depends("gi", "vat")
     def _compute_cgi(self):
@@ -117,13 +179,13 @@ class FlorenceFinancialPlan(models.Model):
             if line.gi and line.vat:
                 line.cgi = line.gi - line.vat
 
-    @api.depends("gi", "vat", "perc")
+    @api.depends("cgi", "perc")
     def _compute_disbursment(self):
         for line in self:
             line.disbursment = 0
 
-            if line.gi and line.vat and line.perc:
-                line.disbursment = line.gi - line.vat - line.perc
+            if line.cgi and line.perc:
+                line.disbursment = line.cgi - line.perc
 
     @api.depends("basics", "emergencies",
                  "div1", "div2", "div3", "div4", "div5", "div6", "div7")
