739443218c8b1f82f938e4f96639bf7be371d520
nickciava98 <n.ciavarella@outlook.it>
2022-11-07 11:34:41 +0100

    Aggiunto numero fattura e data in fp_costs

--- florence_amazon_financial_plan/models/florence_fp_costs_line.py
+++ florence_amazon_financial_plan/models/florence_fp_costs_line.py
@@ -16,10 +16,33 @@ class FlorenceFpCostsLine(models.Model):
     cost = fields.Float(
         digits = (12, 4)
     )
+    bill = fields.Many2one(
+        "account.move",
+        compute = "_compute_bill"
+    )
+    bill_date = fields.Date(
+        related = "bill.invoice_date"
+    )
     currency_id = fields.Many2one(
         "res.currency",
         compute = "_compute_currency_id"
     )
+
+    @api.depends("component")
+    def _compute_bill(self):
+        for line in self:
+            line.bill = False
+
+            if line.component:
+                for bill in self.env["account.move"].search(
+                        [("move_type", "=", "in_invoice")], order="name desc"):
+                    for invoice_line in bill.invoice_line_ids:
+                        if invoice_line.product_id == line.component:
+                            line.bill = bill
+                            break
+
+                    if line.bill:
+                        break
 
     def _compute_currency_id(self):
         for line in self:
