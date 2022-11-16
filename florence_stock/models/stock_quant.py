d893e439a4d42fa273015144ed1aa0968022b8f3
nickciava98 <n.ciavarella@outlook.it>
2022-11-16 09:16:47 +0100

    Fix value stock

--- florence_stock/models/stock_quant.py
+++ florence_stock/models/stock_quant.py
@@ -18,12 +18,13 @@ class StockQuant(models.Model):
             if line.product_id:
                 cost = 0
 
-                for fp in self.env["florence.fp.costs"].search([]):
-                    for fp_line in fp.fp_costs_lines:
-                        if fp_line.component == line.product_id:
-                            cost = fp_line.cost
+                for bill in self.env["account.move"].search(
+                        [("move_type", "=", "in_invoice")], order = "name desc"):
+                    for invoice_line in bill.invoice_line_ids:
+                        if invoice_line.product_id == line.product_id:
+                            cost = invoice_line.price_unit
                             break
-                    if cost != 0:
+                    if cost > 0:
                         break
-                if cost != 0:
+                if cost > 0:
                     line.value = cost * line.available_quantity
