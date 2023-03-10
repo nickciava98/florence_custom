9f19d54213b66d84975398969bfc2604a58780cc
nickciava98 <n.ciavarella@outlook.it>
2023-03-10 12:52:58 +0100

    Added amazon connector's customizations

---
+++ florence_amazon_connector/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Florence Amazon Connector",
+    'summary': "Florence customization for Amazon Connector",
+    'license': 'OPL-1',
+    'author': "Niccolò Ciavarella",
+    'category': 'sales',
+    'version': '14.0.1',
+    'website': "http://www.nciavarella.me",
+    'depends': [
+        'sale_amazon'
+    ],
+    'data': [
+        'views/amazon_connector_menu.xml'
+    ],
+    'application': False,
+    'installable': True,
+}
