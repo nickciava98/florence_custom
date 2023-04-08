fde2f013f05bad726c4e4fbdca237191e2e3f894
nickciava98 <n.ciavarella@outlook.it>
2023-04-08 11:37:42 +0200

    Fix balance sheet

--- florence_amazon_financial_plan/models/florence_balance_sheet_inventory.py
+++ florence_amazon_financial_plan/models/florence_balance_sheet_inventory.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class FlorenceBalanceSheetInventory(models.Model):
@@ -33,6 +33,24 @@ class FlorenceBalanceSheetInventory(models.Model):
         for line in self:
             line.currency_id = self.env.ref('base.main_company').currency_id
 
+    @api.onchange("available_quantity")
+    def _onchange_available_quantity(self):
+        for line in self:
+            if line.product_id:
+                cost = 0
+
+                for bill in self.env["account.move"].search(
+                        [("move_type", "=", "in_invoice")], order="name desc"):
+                    for invoice_line in bill.invoice_line_ids:
+                        if invoice_line.product_id == line.product_id:
+                            cost = invoice_line.price_unit
+                            break
+                    if cost > 0:
+                        break
+                if cost > 0:
+                    line.value = cost * line.available_quantity
+
+
 class FlorenceBalanceSheetInventoryMore(models.Model):
     _name = "florence.balance.sheet.inventory.more"
     _inherit = ["mail.thread", "mail.activity.mixin"]
@@ -60,3 +78,20 @@ class FlorenceBalanceSheetInventoryMore(models.Model):
     def _compute_currency_id(self):
         for line in self:
             line.currency_id = self.env.ref('base.main_company').currency_id
+
+    @api.onchange("available_quantity")
+    def _onchange_available_quantity(self):
+        for line in self:
+            if line.product_id:
+                cost = 0
+
+                for bill in self.env["account.move"].search(
+                        [("move_type", "=", "in_invoice")], order="name desc"):
+                    for invoice_line in bill.invoice_line_ids:
+                        if invoice_line.product_id == line.product_id:
+                            cost = invoice_line.price_unit
+                            break
+                    if cost > 0:
+                        break
+                if cost > 0:
+                    line.value = cost * line.available_quantity
