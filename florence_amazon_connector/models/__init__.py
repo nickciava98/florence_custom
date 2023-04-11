ac4320cc6c2136b69b11d59fc24efd4f04c3a3ba
nickciava98 <n.ciavarella@outlook.it>
2023-04-11 16:42:30 +0200

    Added Sale Amazon Report

--- florence_amazon_connector/models/__init__.py
+++ florence_amazon_connector/models/__init__.py
@@ -1,2 +1,3 @@
 from . import stock_location
 from . import sale_order
+from . import sale_report
---
+++ florence_amazon_connector/models/sale_report.py
@@ -0,0 +1,47 @@
+from odoo import models
+import datetime
+import calendar
+
+
+class SaleReport(models.Model):
+    _inherit = "sale.report"
+
+    def view_amazon_report_action(self):
+        current_year = datetime.datetime.now().year
+        current_month = datetime.datetime.now().month
+        last_day = str(calendar.monthrange(int(current_year), int(current_month))[1])
+
+        return {
+            "name": "Sales Analysis",
+            "res_model": "sale.report",
+            "view_type": "dashboard",
+            "view_mode": "dashboard",
+            "type": "ir.actions.act_window",
+            "context": {
+                "graph": {
+                    "graph_measure": "price_subtotal",
+                    "graph_mode": "line",
+                    "graph_groupbys": ["date:day"]
+                },
+                "pivot": {
+                    "pivot_measures": [
+                        "__count",
+                        "order_id",
+                        "price_subtotal",
+                        "price_total"
+                    ],
+                    "pivot_column_groupby": [],
+                    "pivot_row_groupby": [
+                        "categ_id",
+                        "product_tmpl_id"
+                    ]
+                },
+                "group_by": []
+            },
+            "domain": [
+                "&", "&",
+                ("date", ">=", str(current_year) + "-" + str(current_month) + "-1 00:00:01"),
+                ("date", "<=", str(current_year) + "-" + str(current_month) + "-" + str(last_day) + " 23:59:59"),
+                ("team_id", "ilike", "Amazon")
+            ]
+        }
