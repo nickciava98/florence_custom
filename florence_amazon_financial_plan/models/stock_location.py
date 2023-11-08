431f49197afa46af622cbca8b0224237f0f7b6cb
nickciava98 <n.ciavarella@outlook.it>
2023-11-08 09:46:54 +0100

    Added evidence for valuable stocks

--- florence_amazon_financial_plan/models/stock_location.py
+++ florence_amazon_financial_plan/models/stock_location.py
@@ -7,3 +7,9 @@ class StockLocation(models.Model):
     is_valuable_stock = fields.Boolean(
         default=False
     )
+
+    def name_get(self):
+        return [
+            (name[0], name[1] + " €€") if self.browse(name[0]).is_valuable_stock else name
+            for name in super().name_get()
+        ]
