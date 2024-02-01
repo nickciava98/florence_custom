5d70479f80c8ce2946ce729e1fd3cd59af313208
nickciava98 <n.ciavarella@outlook.it>
2024-02-01 12:40:25 +0100

    Update FP Cost computes

--- florence_amazon_financial_plan/wizard/fp_costs_update.py
+++ florence_amazon_financial_plan/wizard/fp_costs_update.py
@@ -24,39 +24,34 @@ class FpCostsUpdate(models.TransientModel):
         last_curr_day = str(calendar.monthrange(int(year), int(current_month))[1])
         updates = 0
         florence_fp_costs_ids = self.env["florence.fp.costs"].search(
-            ["&",
-             ("date", ">=", year + "-" + prev_month + "-1"),
-             ("date", "<=", year + "-" + prev_month + "-" + last_day)]
+            [("date", ">=", f"{year}-{prev_month}-01"),
+             ("date", "<=", f"{year}-{prev_month}-{last_day}")]
         )
         florence_fp_costs_curr_month_ids = self.env["florence.fp.costs"].search(
-            ["&",
-             ("date", ">=", year + "-" + current_month + "-1"),
-             ("date", "<=", year + "-" + current_month + "-" + last_curr_day)]
+            [("date", ">=", f"{year}-{current_month}-01"),
+             ("date", "<=", f"{year}-{current_month}-{last_curr_day}")]
         )
 
-        if len(florence_fp_costs_ids) > 0 and len(florence_fp_costs_curr_month_ids) == 0:
+        if florence_fp_costs_ids and not florence_fp_costs_curr_month_ids:
             for fp_cost in florence_fp_costs_ids:
                 fp_costs_lines = []
 
                 for fp_cost_line in fp_cost.fp_costs_lines:
-                    invoice_last_date = year + "-" + current_month + "-" + last_curr_day
-                    domain = [
-                        "&",
-                        ("move_type", "=", "in_invoice"),
-                        ("invoice_date", "<=", invoice_last_date)
-                    ]
+                    invoice_last_date = f"{year}-{current_month}-{last_curr_day}"
+                    domain = [("move_type", "=", "in_invoice"), ("invoice_date", "<=", invoice_last_date)]
                     bill_id = 0
                     cost = .0
 
                     for bill in self.env["account.move"].search(domain, order="id desc"):
-                        for invoice_line in bill.invoice_line_ids:
-                            if invoice_line.product_id.id == fp_cost_line.component.id:
-                                bill_id = bill.id
-                                cost = invoice_line.price_unit
-                                break
+                        invoice_line_id = bill.invoice_line_ids.filtered(
+                            lambda l: l.product_id.id == fp_cost_line.component.id
+                        )
 
-                        if bill_id != 0 and not math.isclose(cost, 0.0):
-                            break
+                        if not invoice_line_id:
+                            continue
+
+                        bill_id = bill.id
+                        cost = invoice_line_id[0].price_unit
 
                     fp_costs_lines.append({
                         "component": fp_cost_line.component.id,
@@ -71,7 +66,7 @@ class FpCostsUpdate(models.TransientModel):
                     "pieces": 1
                 })
 
-                if len(fp_costs_lines) > 0:
+                if fp_costs_lines:
                     for fp_cost_line in fp_costs_lines:
                         fp_cost_line["name"] = fp_cost_id.id
 
